31c94801ae3827bb9af9289f9f34d1993c0989bd
MoatazZewin <moatazzewin@gmail.com>
2024-11-20 07:23:12 +0200

    setup envirnoment ,and header and landing sections

---
+++ main.js
@@ -0,0 +1,24 @@
+const btnToggle = document.getElementById("btn-toggle");
+const list = document.getElementById("header-list");
+const listIcon = document.getElementById("icons");
+const children = [...list.children];
+const buttons = document.getElementsByClassName("days");
+const btnContent = [...document.getElementsByClassName("tab-pane")];
+const header = document.getElementsByTagName("header")[0];
+
+const sections = document.querySelectorAll("section");
+
+btnToggle.addEventListener("click", (event) => {
+  list.classList.toggle("show");
+  listIcon.classList.toggle("show");
+  btnToggle.ariaExpanded = !JSON.parse(btnToggle.ariaExpanded);
+});
+
+let current = "";
+window.addEventListener("scroll", () => {
+  if (window.scrollY == 0) {
+    header.classList.remove("sticky");
+  } else {
+    header.classList.add("sticky");
+  }
+});
